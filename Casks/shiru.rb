1f4c9d7a39bc03ba1429efad2400313883e92ae0
remusa <remusa@users.noreply.github.com>
2026-03-29 23:42:09 -0600

    style(shiru): convert to parentheses syntax and bump to 6.5.2

--- Casks/shiru.rb
+++ Casks/shiru.rb
@@ -1,22 +1,23 @@
-cask "shiru" do
-  version "6.5.2"
-  sha256 "3f65324e5236ce037db66a76b5c6c4a25565fbeba0f00d0139f02bfabbf3ca51"
+cask("shiru") do
+  version("6.5.2")
+  sha256("3f65324e5236ce037db66a76b5c6c4a25565fbeba0f00d0139f02bfabbf3ca51")
 
-  # Updated to match the new "mac-Shiru-v{version}.dmg" format
-  url "https://github.com/RockinChaos/Shiru/releases/download/v#{version}/mac-Shiru-v#{version}.dmg"
-  name "Shiru"
-  desc "Specialized audio/visual tool"
-  homepage "https://github.com/RockinChaos/Shiru"
+  url("https://github.com/RockinChaos/Shiru/releases/download/v#{version}/mac-Shiru-v#{version}.dmg")
+  name("Shiru")
+  desc("Specialized audio/visual tool")
+  homepage("https://github.com/RockinChaos/Shiru")
 
   livecheck do
-    url "https://github.com/RockinChaos/Shiru/releases/latest"
-    strategy :github_latest
+    url("https://github.com/RockinChaos/Shiru/releases/latest")
+    strategy(:github_latest)
   end
 
-  app "Shiru.app"
+  app("Shiru.app")
 
-  zap trash: [
-    "~/Library/Application Support/Shiru",
-    "~/Library/Preferences/com.rockinchaos.shiru.plist",
-  ]
+  zap(
+    trash: [
+      "~/Library/Application Support/Shiru",
+      "~/Library/Preferences/com.rockinchaos.shiru.plist"
+    ]
+  )
 end
