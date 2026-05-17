491da6b82a3f7c6a55c3474e3237add6fcb2b313
remusa <remusa@users.noreply.github.com>
2026-05-17 02:14:43 +0000

    fix(typewhisper): update to plugin-parakeet-1.2.14

--- Casks/typewhisper.rb
+++ Casks/typewhisper.rb
@@ -1,5 +1,5 @@
 cask("typewhisper") do
-  version("plugin-lietranscript-v1.0.10")
+  version("plugin-parakeet-1.2.14")
   sha256("0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
 
   url(
