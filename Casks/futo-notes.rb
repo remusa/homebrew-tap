1622b1aa7630380178ebb9248d1ff3ccd702424e
RMS <s4mxdhid@zurimail.org>
2026-07-29 21:21:06 -0600

    fix(futo-notes): add missing v prefix in package URL path

--- Casks/futo-notes.rb
+++ Casks/futo-notes.rb
@@ -2,7 +2,7 @@ cask("futo-notes") do
   version("1.6.1")
   sha256("9491d4737000e80bcbdd7a39e9dc13c2178ff865beff7d800d6159bfc395e8fa")
 
-  url("https://gitlab.futo.org/api/v4/projects/488/packages/generic/futo-notes/#{version}/FUTO-Notes-#{version}-universal.dmg",
+  url("https://gitlab.futo.org/api/v4/projects/488/packages/generic/futo-notes/v#{version}/FUTO-Notes-#{version}-universal.dmg",
       verified: "gitlab.futo.org")
   name("FUTO Notes")
   desc("Notes, but smart")
